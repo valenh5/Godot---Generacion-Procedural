d77bf2905b03f62cc8953571d48e1d0832cc130b
valenh5 <alumno25.huerga.valentin@ipm.edu.ar>
2025-06-14 12:20:40 -0300

    Add files via upload
    Mejor optimizacion

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,3 @@
-# Script del jugador
 extends CharacterBody2D
 
 const SPEED = 300.0
@@ -6,25 +5,24 @@ const JUMP_VELOCITY = -400.0
 
 @onready var tilemap = $"../world"
 
-@onready var chunkPos = Vector2(position.x / (tilemap.chunk_size * 20), position.y / (tilemap.chunk_size * 20))
+@onready var chunkPos = Vector2i(position.x / (tilemap.chunk_size * 16), position.y / (tilemap.chunk_size * 16))
 @onready var prevchunkPos = chunkPos
 
 func _physics_process(delta: float) -> void:
-	chunkPos = Vector2(position.x / (tilemap.chunk_size * 20), position.y / (tilemap.chunk_size * 20))
+	chunkPos = Vector2i(position.x / (tilemap.chunk_size * 16), position.y / (tilemap.chunk_size * 16))
 
 	if prevchunkPos != chunkPos:
 		tilemap.updateChunk(chunkPos)
 		prevchunkPos = chunkPos
 
-	# Aplicar gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Manejo de salto
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Manejo de movimiento
+
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
--- scripts/world.gd
+++ scripts/world.gd
@@ -41,7 +41,7 @@ func _ready():
 	mountain_spacing = randi_range(montania_rango.x, montania_rango.y)
 	mountain_offset = randi_range(0, mountain_spacing - 1)
 
-	generateWorld(Vector2(0, 0))
+	generateWorld(Vector2i(0, 0))
 
 func obtener_rango_desde_valor(valor: int) -> Vector2i:
 	match valor:
@@ -57,7 +57,7 @@ func obtener_rango_desde_valor(valor: int) -> Vector2i:
 func updateChunk(chunk_pos):
 	for x in range(-chunk_cant, chunk_cant):
 		for y in range(-chunk_cant, chunk_cant):
-			var current_pos = chunk_pos + Vector2(x, y)
+			var current_pos = chunk_pos + Vector2i(x, y)
 			if chunk_status.get(current_pos, false) != true:
 				loadChunk(current_pos)
 				
@@ -71,9 +71,9 @@ func saveChunks(chunk_pos):
 	var chunk_tiles = []
 	for x in range(start_x, start_x + chunk_size):
 		for y in range(start_y, start_y + chunk_size):
-			var cell = get_cell_atlas_coords(Vector2(x, y))
+			var cell = get_cell_atlas_coords(Vector2i(x, y))
 			if cell != null:
-				chunk_tiles.append({"pos": Vector2(x, y), "tile": cell})
+				chunk_tiles.append({"pos": Vector2i(x, y), "tile": cell})
 	chunk_data[chunk_pos] = chunk_tiles
 
 func loadChunk(chunk_pos):
@@ -99,12 +99,12 @@ func unloadChunk(chunk_pos):
 
 func colocar_arbol(pos_x: int, pos_y: int):
 	for i in range(TRONCO_ALTURA):
-		set_cell(Vector2(pos_x, pos_y - i), 0, TRONCO_TILE)
+		set_cell(Vector2i(pos_x, pos_y - i), 0, TRONCO_TILE)
 	for i in range(-1, 2):
 		for j in range(-1, 2):
-			set_cell(Vector2(pos_x + i, pos_y - TRONCO_ALTURA + j), 0, HOJAS_TILE)
+			set_cell(Vector2i(pos_x + i, pos_y - TRONCO_ALTURA + j), 0, HOJAS_TILE)
 
-func generateWorld(chunk_pos : Vector2):
+func generateWorld(chunk_pos : Vector2i):
 	var start_x = chunk_pos.x * chunk_size
 	var base_height = 5
 	var protected_range = 10
@@ -112,11 +112,11 @@ func generateWorld(chunk_pos : Vector2):
 	for x in range(start_x, start_x + chunk_size):
 		var height_variation = noise.get_noise_2d(x, 0) * 5
 		var terrain_height = base_height + height_variation
-		var terrain_pos = Vector2(x, terrain_height)
+		var terrain_pos = Vector2i(x, terrain_height)
 
 		set_cell(terrain_pos, 0, PASTO_TILE)
 		for y in range(terrain_height + 1, terrain_height + chunk_size):
-			set_cell(Vector2(x, y), 0, TIERRA_TILE)
+			set_cell(Vector2i(x, y), 0, TIERRA_TILE)
 
 		var mountain_value = mountain_noise.get_noise_2d(x, 0)
 		if abs(x) > protected_range and mountain_value > 0.6 and x % mountain_spacing == mountain_offset:
@@ -125,8 +125,8 @@ func generateWorld(chunk_pos : Vector2):
 			for i in range(mountain_height):
 				var width = mountain_height - i
 				for j in range(-width, width + 1):    
-					set_cell(Vector2(x + j, terrain_height - i), 0, TIERRA_TILE)
-			set_cell(Vector2(x, mountain_top), 0, PASTO_TILE)
+					set_cell(Vector2i(x + j, terrain_height - i), 0, TIERRA_TILE)
+			set_cell(Vector2i(x, mountain_top), 0, PASTO_TILE)
 			continue
 
 		if abs(x) > protected_range and x % tree_spacing == tree_offset:
